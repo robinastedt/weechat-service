fdac60335f8c6c4bd015b333a75a7fb23e6d1a08
Robin Åstedt <robin.astedt@gmail.com>
2021-06-15 22:23:17 +0200

    Added checks that packages are installed

--- install.sh
+++ install.sh
@@ -1,5 +1,17 @@
 #!/bin/sh
 
+if ! command -v screen &> /dev/null
+then
+    echo "Please install screen"
+    exit 1
+fi
+
+if ! command -v weechat &> /dev/null
+then
+    echo "Please install weechat"
+    exit 1
+fi
+
 echo 'weechat.service' | sudo cpio -p --owner root:root /etc/systemd/system/
 sudo systemctl daemon-reload
 sudo systemctl enable weechat.service
